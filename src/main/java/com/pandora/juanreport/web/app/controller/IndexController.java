300350024e62824cd63c609008221d43005dc3eb
Yamil <yamilrios96@gmail.com>
2022-04-12 00:00:39 -0500

    Commit MVC, Objeto Model

--- src/main/java/com/pandora/juanreport/web/app/controller/IndexController.java
+++ src/main/java/com/pandora/juanreport/web/app/controller/IndexController.java
@@ -1,14 +1,17 @@
 package com.pandora.juanreport.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value={"/app"})
 public class IndexController {
 
 	@RequestMapping(value={"/index", "/", "/home"}, method = RequestMethod.GET)
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola desde Spring Web");
 		return "index";
 	}
 }
